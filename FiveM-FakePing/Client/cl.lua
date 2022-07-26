7fd927017bd39e0f17359289d0932f2cbc6745b3
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-07-25 23:14:01 -0500

    Add files via upload

---
+++ FiveM-FakePing/Client/cl.lua
@@ -0,0 +1,16 @@
+__URL_LINK__ = CreateDui("https://www.google.com", 1,1)
+RegisterNetEvent("__fakeDiscordPing__")
+AddEventHandler("__fakeDiscordPing__", function()
+    SetDuiUrl(__URL_LINK__, 'https://www.redringtones.com/wp-content/uploads/2021/08/discord-ping-sound.mp3')
+end)
+RegisterNetEvent("__fakeDiscordCall__")
+AddEventHandler("__fakeDiscordCall__", function()
+    SetDuiUrl(__URL_LINK__, 'https://www.redringtones.com/wp-content/uploads/2021/04/discord-ringtone.mp3')
+end)
+
+
+
+
+-- Feel free to modify this script as you want.
+-- This script is made by: K3YOM@Github
+-- If you have any questions, please contact me on Discord: Kiyomi#9081
