914ce9f2766a2f45042359f7cfbd94651abd1abe
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-07-25 23:13:56 -0500

    Delete FiveM-FakePing directory

--- FiveM-FakePing/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-author 'K3YOMI@Github'
-description '[K3YOMI] Simple troll discord noise effects to troll your players'
-game 'common'
-fx_version 'adamant'
-server_scripts {'Server/*.*'}
-client_scripts {'Client/*.*'}
